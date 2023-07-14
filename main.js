83357dcfaab86959640c969bcc334ae7c35919ac
Dang Thanh Dat <dangthanhdatuit@gmail.com>
2023-07-15 01:19:35 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,13 @@
+function to_open_NamLe()
+{
+    window.location.href = "/NamLe.html"
+}
+
+function to_open_HoangNam()
+{
+    window.location.href = "/HoangNam.html"
+}
+
+function closeWebsite() {
+    window.close();
+}
